43f6e7af04c24fc8e16f3bd8d519a4f61feaca69
aditya <adityask2194@gmail.com>
2021-09-09 23:13:17 +0100

    final commit v3

--- src/main.js
+++ src/main.js
@@ -12,12 +12,7 @@ import { initializeApp } from "firebase/app";
 
 const Title = defineAsyncComponent(()=>import(/*webpackChunkName:"title"*/ '@/components/atoms/Title.vue'))
 initializeApp({
-    apiKey: "AIzaSyAN1kiXNbSPIi_lGu6z85hg4OvvfnFNM7M",
-    authDomain: "vue-course-c6ed3.firebaseapp.com",
-    projectId: "vue-course-c6ed3",
-    storageBucket: "vue-course-c6ed3.appspot.com",
-    messagingSenderId: "741597903366",
-    appId: "1:741597903366:web:082bf24a0e238074bea187"
+  // you firbase config
   });
 
 createApp(App).use(store).use(router).use(head).component('Title', Title).mount('#app')
